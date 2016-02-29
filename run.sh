d8c438b108f74ead35ecce2082947d329a5eb1a8
jospergon <josemanuelprz@gmail.com>
2016-02-29 15:03:54 +0100

    indent nginx.conf file.

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo $UPSTREAM_SERVER
-
-sed "s/UPSTREAM_SERVER/$UPSTREAM_SERVER/g" /etc/nginx/nginx.conf
+sed -i "s/UPSTREAM_SERVER/$UPSTREAM_SERVER/g" /etc/nginx/nginx.conf
+sed -i "s/UPSTREAM_PORT/$UPSTREAM_PORT/g" /etc/nginx/nginx.conf
 nginx -g "daemon off;"
